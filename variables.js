c81404f767ba623eaf3665ba4e68962dcd369e61
raghavareddy <raghavareddy2325@gmail.com>
2022-08-16 23:31:50 +0530

    code changed

--- variables.js
+++ variables.js
@@ -1,2 +1,15 @@
 var x;
 console.log(x);
+// global
+let day = "Monday";
+let color = "Green";
+if(day=="Monday")
+{
+    let color = "Blue";
+    console.log("it is monday.The sky is of" +color);
+}
+// local
+var fullname="RAGHAVA";
+
+console.log("My name is:")
+console.log(fullname);
